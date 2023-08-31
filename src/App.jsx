e8859e4bff122f4578db4063d58682f5946d2a1d
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-08-31 17:40:30 -0400

    add .gitignore, add .env secret key, add to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import OpenAI from "openai";
 import "./App.css";
 
 function App() {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+});
   return <div></div>;
 }
 
