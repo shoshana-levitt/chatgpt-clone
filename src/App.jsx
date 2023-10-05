cfb19e8c7ec961470962ae7423eb3a9debc2b923
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-10-05 15:21:09 -0400

    Add Hello World, get rid of await in chatCompletion

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ function App() {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  const chatCompletion = await openai.chat.completions.create({
+
+  const chatCompletion = openai.chat.completions.create({
     messages: [{ role: "user", content: "Say this is a test" }],
     model: "gpt-3.5-turbo",
-});
-  return <div></div>;
+  });
+  return <div>Hello World</div>;
 }
 
 export default App;
