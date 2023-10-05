650e7baa923137a2788002887694ab58a5b543d1
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-10-05 15:59:35 -0400

    add generateImage func, not working, button not appearing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import OpenAI from "openai";
+import { OpenAI } from "openai";
 import "./App.css";
 
 function App() {
@@ -7,11 +7,28 @@ function App() {
     apiKey: process.env.OPENAI_API_KEY,
   });
 
-  const chatCompletion = openai.chat.completions.create({
-    messages: [{ role: "user", content: "Say this is a test" }],
-    model: "gpt-3.5-turbo",
-  });
-  return <div>Hello World</div>;
+  //   const chatCompletion = openai.chat.completions.create({
+  //     messages: [{ role: "user", content: "Say this is a test" }],
+  //     model: "gpt-3.5-turbo",
+  //   });
+
+  const generateImage = async () => {
+    console.log("generateImage function called");
+    const response = await openai.createImage({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024",
+    });
+    const image_url = response.data.data[0].url;
+    console.log(image_url);
+    return image_url;
+  };
+
+  return (
+    <div>
+      <button onClick={generateImage}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
